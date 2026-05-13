f67db8f5599837e0dd1a23267ae2a8f6242f090a
Omaroviz <omaroviz1990@gmail.com>
2026-05-13 09:43:35 +0300

    initial commit

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,19 @@
 'use strict'
 
-const userName = document.getElementById("profileName")
+// const userName = document.getElementById("profileName")
+//
+// userName.innerText = "userName"
 
-userName.innerText = "userName"
+let num;
+
+num = Math.max("8", "9")
+
+let ceilnum = Math.ceil(1.9);
+let floornum = Math.floor(1.1);
+let roundnum = Math.round(1.9)
+
+console.log(num)
+console.log(ceilnum)
+console.log(floornum)
+console.log(roundnum)
+// console.log(a)
