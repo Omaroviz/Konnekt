660420d4430e3eeb41dfa8236fd724d8de998e9f
Omaroviz <omaroviz1990@gmail.com>
2026-05-15 18:32:28 +0300

    Скоро полное обновление

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,19 +1,53 @@
 'use strict'
+//
+// // const userName = document.getElementById("profileName")
+// //
+// // userName.innerText = "кто-то"
+//
+//
+// let num;
+//
+// num = Math.max("8", "9")
+//
+// let ceilnum = Math.ceil(1.9);
+// let floornum = Math.floor(1.1);
+// let roundnum = Math.round(1.9)
+//
+// console.log(num)
+// console.log(ceilnum)
+// console.log(floornum)
+// console.log(roundnum)
+// // console.log(a)
 
-const userName = document.getElementById("profileName")
+const algorithm = (arr, item) => {
+	let low = 0;
+	let high = arr.length - 1;
+	let count = 0;
 
-userName.innerText = "кто-то"
+	while (low <= high) {
+		let mid = Math.floor((low + high) / 2);
+		let guest = arr[mid];
 
-let num;
+		++count;
 
-num = Math.max("8", "9")
+		console.log(`--------------\nCOUNT: ${count} (Начало)\nlow = ${low}\nhigh = ${high}\nmid = ${mid}\nguest = ${guest}`)
 
-let ceilnum = Math.ceil(1.9);
-let floornum = Math.floor(1.1);
-let roundnum = Math.round(1.9)
 
-console.log(num)
-console.log(ceilnum)
-console.log(floornum)
-console.log(roundnum)
-// console.log(a)
+		if (guest === item) {
+			return mid
+		} else if (guest > item) {
+			high = mid-1;
+		} else {
+			low = mid+1;
+		}
+
+		++count;
+
+		console.log(`--------------\nCOUNT: ${count} (Конец)\nlow = ${low}\nhigh = ${high}\nmid = ${mid}\nguest = ${guest}`)
+
+	}
+
+	return null;
+}
+
+console.log(algorithm([1, 2, 3, 4, 5, 6, 7, 8, 9], 7));
